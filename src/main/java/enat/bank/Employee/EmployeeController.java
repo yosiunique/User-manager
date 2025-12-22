4472bf41e22565d0339edd16a1336f6f9de9ce2b
Fentahun Amare <fentahun.amare@enatbanksc.com>
2025-12-22 12:43:08 +0000

    Edit EmployeeController.java

--- src/main/java/enat/bank/Employee/EmployeeController.java
+++ src/main/java/enat/bank/Employee/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.MediaType;
 
 import java.security.PublicKey;
 import java.time.LocalDate;
