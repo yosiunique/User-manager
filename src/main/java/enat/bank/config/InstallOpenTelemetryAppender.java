8c6c50250000fbcff8f37e4335f2c693bc40d985
getachewy307@gmail.com <getachewy307@gmail.com>
2026-06-26 17:52:25 +0300

    this was slightly changes

---
+++ src/main/java/enat/bank/config/InstallOpenTelemetryAppender.java
@@ -0,0 +1,22 @@
+package enat.bank.config;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+ class InstallOpenTelemetryAppender implements InitializingBean {
+
+    private final OpenTelemetry openTelemetry;
+
+    InstallOpenTelemetryAppender(OpenTelemetry openTelemetry) {
+        this.openTelemetry = openTelemetry;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        OpenTelemetryAppender.install(this.openTelemetry);
+    }
+}
